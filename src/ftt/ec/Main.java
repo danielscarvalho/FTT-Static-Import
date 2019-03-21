3be630cb599b853da2caae11e4f9962a22d9de36
engineer <engineer@FTT-EC-VirtualBox>
2019-03-20 21:46:02 -0300

    First code

---
+++ src/ftt/ec/Main.java
@@ -0,0 +1,25 @@
+package ftt.ec;
+/*****
+ * 
+ * Reference:
+ * 
+ * https://blog.alura.com.br/como-fazer-um-import-static-em-java/
+ * https://docs.oracle.com/javase/8/docs/technotes/guides/language/static-import.html
+ * 
+ */
+import static java.lang.Math.*;
+import static java.lang.System.out;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		out.print("Vai corinthians!!");
+		out.println();
+		
+		out.println(sin(1)+cos(.5));
+		
+	}
+
+}
